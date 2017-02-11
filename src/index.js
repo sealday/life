16d806048aeafdd47ea42315d4ba458d3029f0d6
sealday <sealday@gmail.com>
2017-02-11 23:51:30 +0800

    增加redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+
+import * as reducers from './reducers'
+
+const store = createStore(
+  combineReducers(reducers),
+  window['__REDUX_DEVTOOLS_EXTENSION__'] && window['__REDUX_DEVTOOLS_EXTENSION__']()
+);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers.js
@@ -0,0 +1,7 @@
+/**
+ * Created by seal on 11/02/2017.
+ */
+
+export function app(state = {}, action) {
+  return state
+}
