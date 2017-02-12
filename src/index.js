23dce5eaf787a0f2cc399111a20de78acae5a181
sealday <sealday@gmail.com>
2017-02-12 22:53:53 +0800

    整理css，去除无用的部分，改善格式

--- src/index.js
+++ src/index.js
@@ -22,12 +22,12 @@ ReactDOM.render(
   <Provider store={store}>
     <div id="outer-container" style={{ height: '100%' }}>
       <SlideMenu pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
-        <h2 key="0"><i className="fa fa-fw fa-inbox fa-2x" /><span>Sidebar</span></h2>
-        <a key="1" href=""><i className="fa fa-fw fa-database" /><span>Data Management</span></a>
-        <a key="2" href=""><i className="fa fa-fw fa-map-marker" /><span>Location</span></a>
-        <a key="3" href=""><i className="fa fa-fw fa-mortar-board" /><span>Study</span></a>
-        <a key="4" href=""><i className="fa fa-fw fa-picture-o" /><span>Collections</span></a>
-        <a key="5" href=""><i className="fa fa-fw fa-money" /><span>Credits</span></a>
+        <a key="0" href=""><i className="fa fa-fw fa-star-o" /><span>Favorites</span></a>
+        <a key="1" href=""><i className="fa fa-fw fa-bell-o" /><span>Alerts</span></a>
+        <a key="2" href=""><i className="fa fa-fw fa-envelope-o" /><span>Messages</span></a>
+        <a key="3" href=""><i className="fa fa-fw fa-comment-o" /><span>Comments</span></a>
+        <a key="4" href=""><i className="fa fa-fw fa-bar-chart-o" /><span>Analytics</span></a>
+        <a key="5" href=""><i className="fa fa-fw fa-newspaper-o" /><span>Reading List</span></a>
       </SlideMenu>
       <main id="page-wrap">
         <App/>
