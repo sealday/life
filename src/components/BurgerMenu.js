654e76d1fb798123fd7bc37ab2ef0586498a1e65
sealday <sealday@gmail.com>
2017-02-12 22:06:06 +0800

    菜单测试

---
+++ src/components/BurgerMenu.js
@@ -0,0 +1,21 @@
+import slide from './menus/slide'
+import stack from './menus/stack'
+import elastic from './menus/elastic'
+import bubble from './menus/bubble'
+import push from './menus/push'
+import pushRotate from './menus/pushRotate'
+import scaleDown from './menus/scaleDown'
+import scaleRotate from './menus/scaleRotate'
+import fallDown from './menus/fallDown'
+
+export default {
+  slide,
+  stack,
+  elastic,
+  bubble,
+  push,
+  pushRotate,
+  scaleDown,
+  scaleRotate,
+  fallDown,
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
+import BurgerMenu from './components/BurgerMenu'
+
 import * as reducers from './reducers'
+
+import 'normalize.css/normalize.css'
+import 'font-awesome/css/font-awesome.css'
+import './index.css';
 
 const store = createStore(
   combineReducers(reducers),
@@ -15,7 +20,21 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <div id="outer-container" style={{ height: '100%' }}>
+      <div style={{ hidden: true }}>
+        <BurgerMenu.slide pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
+          <h2 key="0"><i className="fa fa-fw fa-inbox fa-2x" /><span>Sidebar</span></h2>
+          <a key="1" href=""><i className="fa fa-fw fa-database" /><span>Data Management</span></a>
+          <a key="2" href=""><i className="fa fa-fw fa-map-marker" /><span>Location</span></a>
+          <a key="3" href=""><i className="fa fa-fw fa-mortar-board" /><span>Study</span></a>
+          <a key="4" href=""><i className="fa fa-fw fa-picture-o" /><span>Collections</span></a>
+          <a key="5" href=""><i className="fa fa-fw fa-money" /><span>Credits</span></a>
+        </BurgerMenu.slide>
+      </div>
+      <main id="page-wrap">
+        <App/>
+      </main>
+    </div>
   </Provider>,
   document.getElementById('root')
 );
