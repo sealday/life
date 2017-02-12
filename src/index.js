605513b89c3507d0430349b91939fb97687c6490
sealday <sealday@gmail.com>
2017-02-12 22:23:03 +0800

    修复百分比问题

--- src/index.js
+++ src/index.js
@@ -21,16 +21,14 @@ const store = createStore(
 ReactDOM.render(
   <Provider store={store}>
     <div id="outer-container" style={{ height: '100%' }}>
-      <div style={{ hidden: true }}>
-        <BurgerMenu.slide pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
-          <h2 key="0"><i className="fa fa-fw fa-inbox fa-2x" /><span>Sidebar</span></h2>
-          <a key="1" href=""><i className="fa fa-fw fa-database" /><span>Data Management</span></a>
-          <a key="2" href=""><i className="fa fa-fw fa-map-marker" /><span>Location</span></a>
-          <a key="3" href=""><i className="fa fa-fw fa-mortar-board" /><span>Study</span></a>
-          <a key="4" href=""><i className="fa fa-fw fa-picture-o" /><span>Collections</span></a>
-          <a key="5" href=""><i className="fa fa-fw fa-money" /><span>Credits</span></a>
-        </BurgerMenu.slide>
-      </div>
+      <BurgerMenu.slide pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
+        <h2 key="0"><i className="fa fa-fw fa-inbox fa-2x" /><span>Sidebar</span></h2>
+        <a key="1" href=""><i className="fa fa-fw fa-database" /><span>Data Management</span></a>
+        <a key="2" href=""><i className="fa fa-fw fa-map-marker" /><span>Location</span></a>
+        <a key="3" href=""><i className="fa fa-fw fa-mortar-board" /><span>Study</span></a>
+        <a key="4" href=""><i className="fa fa-fw fa-picture-o" /><span>Collections</span></a>
+        <a key="5" href=""><i className="fa fa-fw fa-money" /><span>Credits</span></a>
+      </BurgerMenu.slide>
       <main id="page-wrap">
         <App/>
       </main>
